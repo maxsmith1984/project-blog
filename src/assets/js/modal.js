598dbd3662112079ce77b188468da49080eeb7e6
Maks <killingtime2101@mail.ru>
2023-02-08 23:00:47 +0700

    project 0.95 added js for modal log

---
+++ src/assets/js/modal.js
@@ -0,0 +1,46 @@
+const modalBtn = document.querySelectorAll('[data-modal]');
+const body = document.body;
+const modalClose = document.querySelectorAll('.modal__close')
+const modal = document.querySelectorAll('.modal')
+
+modalBtn.forEach(item => {
+    item.addEventListener('click', event => {
+        let $this = event.currentTarget;
+        let modalId = $this.getAttribute('data-modal');
+        let modal = document.getElementById(modalId);
+        let modalContent = modal.querySelector('.modal__content');
+
+        modalContent.addEventListener('click', event => {
+            event.stopPropagation();
+        });
+
+        modal.classList.add('show');
+        body.classList.add('no-scroll');
+
+    });
+});
+
+modalClose.forEach(item => {
+    item.addEventListener('click', event => {
+        let currentModal = event.currentTarget.closest('.modal');
+
+        closeModal(currentModal);
+
+    });
+});
+
+modal.forEach(item => {
+    item.addEventListener('click', event => {
+        let currentModal = event.currentTarget;
+
+        closeModal(currentModal);
+
+    });
+});
+
+function closeModal(currentModal) {
+    currentModal.classList.remove('show');
+    body.classList.remove('no-scroll');
+};
+
+
