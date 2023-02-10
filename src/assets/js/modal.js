49c6c72e0cbc7a0eb002af2caaf1a476943ab2e2
Maks <killingtime2101@mail.ru>
2023-02-10 20:09:20 +0700

    project 1.0 added modal window resize textarea

--- src/assets/js/modal.js
+++ src/assets/js/modal.js
@@ -1,7 +1,8 @@
 const modalBtn = document.querySelectorAll('[data-modal]');
 const body = document.body;
-const modalClose = document.querySelectorAll('.modal__close')
-const modal = document.querySelectorAll('.modal')
+const modalClose = document.querySelectorAll('.modal__close');
+const modal = document.querySelectorAll('.modal');
+const modalVideo = document.querySelectorAll('.modal__video');
 
 modalBtn.forEach(item => {
     item.addEventListener('click', event => {
@@ -16,6 +17,11 @@ modalBtn.forEach(item => {
 
         modal.classList.add('show');
         body.classList.add('no-scroll');
+
+        setTimeout(function () {
+            modalContent.style.transform = 'none';
+        }, 1);
+
 
     });
 });
@@ -35,12 +41,23 @@ modal.forEach(item => {
 
         closeModal(currentModal);
 
+
     });
 });
 
 function closeModal(currentModal) {
-    currentModal.classList.remove('show');
-    body.classList.remove('no-scroll');
+    let modalContent = currentModal.querySelector('.modal__content');
+    modalContent.removeAttribute('style');
+
+    Array.prototype.forEach.call(modalVideo, function (event) {
+        event.pause();
+        event.currentTime = 0;
+    });
+
+    setTimeout(() => {
+        currentModal.classList.remove('show');
+        body.classList.remove('no-scroll');
+    }, 200);
 };
 
 
